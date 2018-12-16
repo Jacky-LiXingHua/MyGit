a59304ed10466b26d436b7cdcc1a2fe757fad063
Jacky-LiXingHua <41607572+Jacky-LiXingHua@users.noreply.github.com>
2018-12-16 15:32:35 +0800

    Update run.sh

--- models-Lixinghua/research/run.sh
+++ models-Lixinghua/research/run.sh
@@ -32,11 +32,11 @@ fig_pb_path=$output_dir/exported_graphs/$fig_pb
 cp $dataset_dir/$config $pipeline_config_path
 cp $dataset_dir/$fig_pb $fig_pb_path
 
-for i in {0..1}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
+for i in {0..4}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
     echo "############" $i "runnning #################"
-    last=$[$i*1]
-    current=$[($i+1)*1]
+    last=$[$i*100]
+    current=$[($i+1)*100]
     sed -i "s/^  num_steps: $last$/  num_steps: $current/g" $pipeline_config_path  # 通过num_steps控制一次训练最多100step
 
     echo "############" $i "training #################"
